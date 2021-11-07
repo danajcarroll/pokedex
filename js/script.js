c6b97ad0aaefba1829b82811fd854c99e27a5aa1
Dana <danacarroll2000@gmail.com>
2021-11-07 08:09:18 -0500

    Created function that calls Pokemon 1's json file to the console log
    '

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+// Using fetch api to get information about the pokemon
+const fetchPokemon = () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/1';
+    fetch(url)
+        .then( res => {
+            return res.json();
+        })
+        .then ( data => {
+            console.log(data);
+        })
+};
+
+fetchPokemon();
