92e91284868094c7998e0107cb903f6789a545cd
Dana <danacarroll2000@gmail.com>
2021-11-07 09:53:01 -0500

    Changed function to list all 150 pokemon parallel instead of one by one using promises

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 // Using fetch api to get information about the pokemon
 const fetchPokemon = () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/1';
-    fetch(url)
-        .then( res => {
-            return res.json();
-        })
-        .then ( data => {
-            console.log(data);
-        })
+// empty array of promises
+const promises = [];
+
+// for each one of our requests, we push that promises INTO our array of promises
+    for (i = 1; i <= 150; i++){
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+}
+// promise.all will let all of these asyncronous calls run in parallel, same time instead of one after the other
+Promise.all(promises).then((results) => {
+    const pokemon = results.map((data) => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites['front_default'],
+        type: data.types.map((type) => type.type.name).join(', ')
+    }));
+    displayPokemon(pokemon);
+});
 };
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+}
 
 fetchPokemon();
